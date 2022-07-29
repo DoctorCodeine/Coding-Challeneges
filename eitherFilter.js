b7ae6d3acc8bf389bc523d8d0fb863fa6116d213
Aimee Nguyen <106037255+DoctorCodeine@users.noreply.github.com>
2022-07-28 20:44:39 -0500

    eitherFilter Challenege
    Add code to the function eitherFilter in the place marked "ADD CODE HERE" in order to achieve the desired console logs. The array returned from eitherFilter should contain all elements in the passed-in array that yield a truthy return value when passed into EITHER of the two callbacks passed into eitherFilter.

--- eitherFilter.js
+++ eitherFilter.js
@@ -1,13 +1,19 @@
 function eitherFilter(array, callback1, callback2) {
-  // ADD CODE HERE
+  
+  //Input: array, callback1, callback2
+  //Output: single array with two elements 
+  
+  //Initalize an empty array to store the output 
   let result = [];
+  
   //Use the array filter method to iterate over each element of the array. 
    array.filter(element => {
+     
      //If the arguement is called by callback 1, push the element into the result array 
     if (callback1(element) || callback2(element)) result.push(element); 
      
-    
   })
+  //Return the output array 
   return result; 
 }
 
