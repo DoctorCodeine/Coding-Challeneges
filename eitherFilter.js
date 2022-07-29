e6b6947ec28570e7b6a9b4aa84a061dc67ef7816
Aimee Nguyen <106037255+DoctorCodeine@users.noreply.github.com>
2022-07-28 20:41:31 -0500

    eitherFilter Challenege
    Add code to the function eitherFilter in the place marked "ADD CODE HERE" in order to achieve the desired console logs. The array returned from eitherFilter should contain all elements in the passed-in array that yield a truthy return value when passed into EITHER of the two callbacks passed into eitherFilter.

---
+++ eitherFilter.js
@@ -0,0 +1,18 @@
+function eitherFilter(array, callback1, callback2) {
+  // ADD CODE HERE
+  let result = [];
+  //Use the array filter method to iterate over each element of the array. 
+   array.filter(element => {
+     //If the arguement is called by callback 1, push the element into the result array 
+    if (callback1(element) || callback2(element)) result.push(element); 
+     //f the arguement is called by callback 1, push the element into the result array 
+    
+  })
+  return result; 
+}
+
+// Uncomment these to check your work!
+const arrOfNums = [10, 35, 105, 9];
+const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
+const over100 = n => n > 100;
+console.log(eitherFilter(arrOfNums, integerSquareRoot, over100)); // should log: [105, 9]
