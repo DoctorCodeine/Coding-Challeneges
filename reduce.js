34d52fcab5655514eab59c84f3196cacf50999ef
Aimee Nguyen <106037255+DoctorCodeine@users.noreply.github.com>
2022-08-14 12:44:42 -0500

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,9 +1,8 @@
 /* 1. Decalre the function called reduce which will take 3 parameters 
 			-> Input: array, callback, startValue
   		-> Output: a number 
-    2. Declare a callback function called reducer which will preform the reduce method. 
-    		paramter: acc, curr -> Paramters will be added to eachother 
-    3. Return the reduce method used on the input array. 
+
+    2. Return the reduce method used on the input array. 
     		parameter: reducer function, callback that will be used on each element of the array, and the start value that is initalized at 0 
 */ 
 const reduce = (array, callback, startValue) => { 
